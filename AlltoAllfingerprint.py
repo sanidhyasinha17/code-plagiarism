bf5bdd3d60bf8b5a1f57380ef172d5150b71adcf
ishaaann <ishanhanda1@gmail.com>
2017-06-26 20:08:55 +0530

    Corrections In All To All Matching

--- AlltoAllfingerprint.py
+++ AlltoAllfingerprint.py
@@ -3,22 +3,26 @@ def stepone(str):
     list3=[]
     str= str.translate(string.maketrans("",""), string.whitespace)
     str= str.translate(string.maketrans("",""), string.punctuation)
-    for t in range(len(str)/9):
-            list3.append(str[t * 9:(t * 9) + 9])
+    for t in range(len(str)-9+1):
+            list3.append(str[t:t+9])
     return list3
-def steptwo(list):
+
+def steptwo(list3):
     list2=[]
-    j=len(list)
+    j=len(list3)
     list1=[]
     for i in range(j):
         h=0
         for k in range(9):
-            h = h + ord(list[i][k]) * pow(3, k)
+            h = h + ord(list3[i][k]) * pow(2, 9-k-1)
         list1.append(h)
+    print list1
+    
     for l in range(j):
         if (list1[l]%4==0):
             list2.append(list1[l])
     return list2
+
 str="fsha aegfaefg srths rtyarhy arha rklhnsg.a.vld;l,hf sfhz s6tnysu4 56a46ub8"
 x=stepone(str)
 print x
